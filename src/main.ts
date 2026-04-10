5105b0b42b502fe3729448bc729ebfd593f4dd61
nudopnu <peter_schramm@live.de>
2026-04-10 16:51:00 +0200

    Fixed publishing issues + screenshot

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
 import { Graph } from './graph.ts'
 
-const graph = new Graph("graph");
+const container = document.querySelector<HTMLElement>("#graph")!;
+const graph = new Graph(container);
 graph.onPlayheadChange(console.log);
